09c83694d63ca40369113a79dde1f0d53ce5fa5c
lushan <shan.lu@mtime.com>
2017-08-23 21:33:02 +0800

    add code

--- app/src/main/java/com/example/ls/kotlindemo/MainActivity.java
+++ app/src/main/java/com/example/ls/kotlindemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.ls.kotlindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = ((TextView) findViewById(R.id.tv));
+        tv.setText("测试");
+
     }
 }
