8bc1d2b086a6ff5a82fdc5bfa0ddd226a48e1b02
lushan <shan.lu@mtime.com>
2017-08-23 22:01:46 +0800

    添加监听

--- app/src/main/java/com/example/ls/kotlindemo/MainActivity.java
+++ app/src/main/java/com/example/ls/kotlindemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ls.kotlindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = ((TextView) findViewById(R.id.tv));
         tv.setText("测试");
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "测试", Toast.LENGTH_SHORT).show();
+
+
+                Toast.makeText(MainActivity.this, "开发", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
